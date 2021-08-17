d48d3db72ed2f0e37c0607908a800253e89ac950
MSPCO <sadeghpoolaee@gmail.com>
2021-08-17 15:53:48 +0430

    added Category controller.

---
+++ SimpleCMS/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleCMS.DataLayer;
+namespace SimpleCMS.Controllers
+{
+    public class CategoryController : Controller
+    {
+        SimpleCMSContext db = new SimpleCMSContext();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return PartialView(db.Categories.ToList());
+        }
+    }
+}
