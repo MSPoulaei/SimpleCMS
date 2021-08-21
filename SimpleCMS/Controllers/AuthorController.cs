8140b50d17756592a4859b1bceacfbe81ca1cbfa
MSPCO <sadeghpoolaee@gmail.com>
2021-08-21 21:30:21 +0430

    added Author controller for viewing author posts.

---
+++ SimpleCMS/Controllers/AuthorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleCMS.DataLayer;
+
+namespace SimpleCMS.Controllers
+{
+    public class AuthorController : Controller
+    {
+        IGenericRepository<Author> authorRepository;
+        SimpleCMSContext db;
+        public AuthorController()
+        {
+            db = new SimpleCMSContext();
+            authorRepository = new GenericRepository<Author>(db);
+        }
+        // GET: Author
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("author/{id}")]
+        public ActionResult AuthorPosts(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            return View("Index", authorRepository.GetById(id.Value).Posts);
+        }
+    }
+}
