46d4daeb2313690995b924c92ee24614fd41e7cf
MSPCO <sadeghpoolaee@gmail.com>
2021-08-23 17:05:32 +0430

    added comment controller to show add comments and subcomments.

---
+++ SimpleCMS/Controllers/CommentController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleCMS.DataLayer;
+
+namespace SimpleCMS.Controllers
+{
+    public class CommentController : Controller
+    {
+        IGenericRepository<Comment> commentRepository;
+        IGenericRepository<SubComment> subcommentRepository;
+        SimpleCMSContext db;
+        public CommentController()
+        {
+            db = new SimpleCMSContext();
+            commentRepository = new GenericRepository<Comment>(db);
+            subcommentRepository = new GenericRepository<SubComment>(db);
+        }
+        // GET: Comment
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowComments(int id)
+        {
+            ViewBag.PersianCalendar = true;
+            return PartialView(db.Comments.Where(c => c.PostId == id));
+        }
+        public ActionResult AddComment(int id,string name,string email,string subject, string message)
+        {
+            Comment newComment = new Comment()
+            {
+                Name = name,
+                Email = email,
+                Title = subject,
+                Text = message,
+                IsAnonymous = true,
+                PostId = id,
+                DateCreated = DateTime.Now,
+                UserId = 2
+            };
+            commentRepository.Insert(newComment);
+            commentRepository.Save();
+            ViewBag.PersianCalendar = true;
+            return PartialView("ShowComments", db.Comments.Where(c => c.PostId == id));
+        }
+        
+        public ActionResult AddSubComment(int id,string name,string email,string subject, string message,int maincommentId)
+        {
+            SubComment newSubComment = new SubComment()
+            {
+                Name = name,
+                Email = email,
+                Title = subject,
+                Text = message,
+                IsAnonymous = true,
+                PostId = id,
+                DateCreated = DateTime.Now,
+                UserId = 2,
+                MainCommentId=maincommentId
+            };
+            subcommentRepository.Insert(newSubComment);
+            subcommentRepository.Save();
+            ViewBag.PersianCalendar = true;
+            return PartialView("ShowComments", db.Comments.Where(c => c.PostId == id));
+        }
+    }
+}
